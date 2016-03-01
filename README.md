6a051fb15d90da9e04320a09bfeb54aad7e4da19
Thomas J Bradley <hey@thomasjbradley.ca>
2016-02-29 19:32:32 -0500

    Initial import

---
+++ README.md
@@ -0,0 +1,28 @@
+# Berry blobber
+
+- Approx. completion time: **1 hour**
+- Deliverables: **1 HTML file, 1 JS file**
+
+---
+
+**Explore using objects and arrays by writing a function to output some berry information.**
+
+- *Fork this repository.*
+- The array of objects is already created for you.
+- Write a function, named `writeBerries`, that will output the information to the document.
+- **DO NOT change the HTML.**
+- *Run it through Markbot and make sure it passes all the checks.*
+
+---
+
+## Goal
+
+Visually match the images in the “screenshots” folder.
+
+- [Final screenshots in the “screenshots” folder.](screenshots)
+
+---
+
+## Hand in
+
+Drop this folder into your Markbot application. Make sure to fix all the errors. And submit to Canvas using Markbot.
---
+++ js/main.js
@@ -0,0 +1,49 @@
+// ##########################################################
+// START DO NOT CHANGE ZONE
+// ##########################################################
+
+var blueberries = {
+  name: 'Blueberries',
+  latin: 'Vaccinium corymbosum',
+  onShrub: true,
+  energy: 240,
+  carbs: 14.49,
+  protein: 0.74
+};
+
+var grapes = {
+  name: 'Grapes',
+  latin: 'Vitis vinifera',
+  onShrub: false,
+  energy: 288,
+  carbs: 18.1,
+  protein: 0.72
+};
+
+var redCurrant = {
+  name: 'Red currant',
+  latin: 'Ribes rubrum',
+  onShrub: true,
+  energy: 234,
+  carbs: 13.8,
+  protein: 1.4
+};
+
+// ##########################################################
+// END DO NOT CHANGE ZONE
+// ##########################################################
+
+
+// Write your function in this space
+
+
+// ##########################################################
+// START DO NOT CHANGE ZONE
+// ##########################################################
+
+// This code will execute your function
+writeBerries([blueberries, grapes, redCurrant]);
+
+// ##########################################################
+// END DO NOT CHANGE ZONE
+// ##########################################################
